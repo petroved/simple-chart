fb865d949f576082c4506dbd976089017cd7bb0a
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2021-07-07 21:53:27 +0000

    Migrate to latest and greatest

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { ReactComponent as Logo } from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
-export const App = (): JSX.Element => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Logo style={{ width: '128px', height: '128px' }} />
-        <p>
-          <code>React ESLint Boilerplate</code>
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    <code>React ESLint Boilerplate</code>
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 };
+
+export default App;
